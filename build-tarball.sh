762f97290f9e4c32df2a0d09a6dd29ba4f658c2d
Bruno Haible <bruno@clisp.org>
2025-01-22 06:00:48 +0100

    Work on check-sanitized.

--- build-tarball.sh
+++ build-tarball.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (C) 2024 Free Software Foundation, Inc.
+# Copyright (C) 2024-2025 Free Software Foundation, Inc.
 #
 # This file is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published
@@ -30,6 +30,7 @@ export GNULIB_SRCDIR=`pwd`/gnulib
 
 # Apply patches.
 (cd "$package" && patch -p1 < ../patches/cygwin32-failure.patch)
+(cd "$package" && patch -p1 < ../patches/ubsan.diff)
 
 cd "$package"
 # Force use of the newest gnulib.
